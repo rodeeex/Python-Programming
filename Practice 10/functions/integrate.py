f80360a3f69d1b3a3847e2f62528c6bb466abc3c
r0dex <adpushkarev@yandex.ru>
2025-12-14 10:54:05 +0300

    Practice 10: added doctests, started report, made threads and proccesses version

--- Practice 10/functions/integrate.py
+++ Practice 10/functions/integrate.py
@@ -1,3 +1,4 @@
+import math
 from typing import Callable
 
 
@@ -11,6 +12,11 @@ def integrate(f: Callable[[float], float], a: float, b: float, *, n_iter: int = 
     :param n_iter: количество разбиений
     :return: приближённое значение интеграла
     :raises ValueError: если a >= b или n_iter <= 0
+
+    >>> abs(integrate(math.sin, 0, math.pi, n_iter=100000) - 2) < 1e-4
+    True
+    >>> abs(integrate(lambda x: x**2, 0, 1, n_iter=100000) - 1/3) < 1e-4
+    True
     """
     if a >= b:
         raise ValueError('Левая граница должна быть меньше правой')
